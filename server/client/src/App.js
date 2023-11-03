8c2922b8a7fc36395d14e52efd75f68509aacb21
Raymond Lui <raymond.lui@stonybrook.edu>
2023-11-03 14:13:50 -0400

    server test

---
+++ server/client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://react.dev">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>GeoWizard Build1</h1>
+    </>
+  )
+}
+
+export default App
