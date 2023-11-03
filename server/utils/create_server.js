85b789adb52bacecd6ff17f50442c5c90ae85540
kahui <kahuiw123@gmail.com>
2023-11-03 00:01:49 -0400

    changed server script from nodemon server/server.js to server/app.js

--- server/utils/create_server.js
+++ server/utils/create_server.js
@@ -17,22 +17,7 @@ const createServer = () => {
     app.use(cors())
     app.use(cookieParser())
     app.use("/users", userRoutes)
-
-    const _dirname = path.dirname("");
-    const buildPath = path.join(_dirname, "../client/build");
-    app.use(express.static(buildPath))
-    app.get("/*", function (req, res) {
-        res.sendFile(
-            path.join(_dirname, "../client/build/index.html"),
-            function (err) {
-                if (err) {
-                    res.status(500).send(err);
-                }
-            }
-        )
-    });
-
-
+    
     return app
 }
 
