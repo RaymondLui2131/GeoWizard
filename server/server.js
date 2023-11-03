9600a4c837311546e7562666f11364a6b96c3dc3
Raymond Lui <raymond.lui@stonybrook.edu>
2023-11-03 01:47:29 -0400

    tried removing get build

--- server/server.js
+++ server/server.js
@@ -10,20 +10,20 @@ const path = require('path')
 const app = express()
 
 
-const _dirname = path.dirname("")
-const buildPath = path.join(_dirname, "../client/build");
-app.use(express.static(buildPath))
+// const _dirname = path.dirname("")
+// const buildPath = path.join(_dirname, "../client/build");
+// app.use(express.static(buildPath))
 
-app.get("/*", function (req, res) {
-    res.sendFile(
-        path.join(_dirname, "../client/build/index.html"),
-        function (err){
-            if (err) {
-                res.status(500).send(err);
-            }
-        }
-    );
-});
+// app.get("/*", function (req, res) {
+//     res.sendFile(
+//         path.join(_dirname, "../client/build/index.html"),
+//         function (err){
+//             if (err) {
+//                 res.status(500).send(err);
+//             }
+//         }
+//     );
+// });
 
 
 require('dotenv').config()
