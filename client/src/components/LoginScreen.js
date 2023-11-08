bb91c836af1d5211a8591da65bf42fb98117088f
Jason <jchengjcheng52@gmail.com>
2023-11-07 21:00:15 -0500

    testing small change login->submit

--- client/src/components/LoginScreen.js
+++ client/src/components/LoginScreen.js
@@ -42,7 +42,7 @@ const LoginScreen = () => {
       <form onSubmit={handleSubmit}>
         <input type="text" name="email" placeholder='Email' value={formData.email} onChange={handleInputChange} />
         <input type="text" name="password" placeholder='Password' value={formData.password} onChange={handleInputChange} />
-        <button type='submit' data-test-id="login-button">Confirm</button>
+        <button type='submit' data-test-id="login-button">Submit</button>
       </form>
       <form onSubmit={handleUser}>
         <input type="text" name="token" placeholder='Enter Token' value={token} onChange={handleTokenChange} />
