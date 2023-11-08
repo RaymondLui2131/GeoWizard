62f1fca216a1326bfb82a386a4ac1432b1a6107c
Jaden <98672622+jadenw2542@users.noreply.github.com>
2023-11-08 01:16:17 -0500

    finished frontend of homescreen

--- client/src/components/HomeScreen.js
+++ client/src/components/HomeScreen.js
@@ -3,17 +3,33 @@ import React, { useState } from 'react'
 //import {authgetUser } from "../auth/auth_request_api"
 //import {useGetUser} from "./UserContext" //updating user via Context jadenw2542@gmail.com
 import Banner from './Banner.js'
-import HomeScreenMapCard from "./HomeScreen"
+import HomeScreenMapCard from "./HomeScreenMapCard.js"
 const HomeScreen = () => {
     //const user = useGetUser()
 
     const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    function handleRecents(){
+        console.log("recents")
+    }
+
+    function handleMostComments(){
+        console.log("comments")
+    }
+
+    function handleTrending(){
+        console.log("tredning")
+    }
+
+    function handlePopular(){
+        console.log("Popular")
+    }
     
     return(
-        <div className="min-h-screen max-h-screen bg-primary-GeoPurple">
+        <div className="min-h-screen max-h-[100%] bg-primary-GeoPurple">
             <Banner/> 
             <div className='flex flex-wrap justify-between items-center mx-auto pt-5 px-20'>
-                
+            
                 <div className= 'text-5xl font-PyeongChangPeace-Light text-primary-GeoBlue' > Popular Maps </div>
 
                 <div className="relative inline-block">
@@ -36,22 +52,31 @@ const HomeScreen = () => {
                     </button>
                     {dropdownOpen && (
                     <div className="absolute w-52 bg-primary-GeoOrange rounded-md shadow-lg ">
-                        <a href="#" className="block px-4 py-2 w-52 font-NanumSquareNeoOTF-Lt text-white bg-primary-GeoOrange hover:bg-primary-GeoBlue rounded-b-none ">Recents</a>
-                        <a href="#" className="block px-4 py-2 w-52 font-NanumSquareNeoOTF-Lt text-white bg-primary-GeoOrange hover:bg-primary-GeoBlue rounded-md" >Most Comments</a>
-                        <a href="#" className="block px-4 py-2 w-52 font-NanumSquareNeoOTF-Lt text-white bg-primary-GeoOrange hover:bg-primary-GeoBlue rounded-md">Trending</a>
-                        <a href="#" className="block px-4 py-2 w-52 font-NanumSquareNeoOTF-Lt text-white bg-primary-GeoOrange hover:bg-primary-GeoBlue rounded-md">Popular</a>
+                        <a onClick={handleRecents} className="block px-4 py-2 w-52 font-NanumSquareNeoOTF-Lt text-white bg-primary-GeoOrange hover:bg-primary-GeoBlue rounded-b-none ">Recents</a>
+                        <a onClick={handleMostComments} className="block px-4 py-2 w-52 font-NanumSquareNeoOTF-Lt text-white bg-primary-GeoOrange hover:bg-primary-GeoBlue rounded-md" >Most Comments</a>
+                        <a onClick={handleTrending} className="block px-4 py-2 w-52 font-NanumSquareNeoOTF-Lt text-white bg-primary-GeoOrange hover:bg-primary-GeoBlue rounded-md">Trending</a>
+                        <a onClick={handlePopular} className="block px-4 py-2 w-52 font-NanumSquareNeoOTF-Lt text-white bg-primary-GeoOrange hover:bg-primary-GeoBlue rounded-md">Popular</a>
 
                     </div>)}
 
                 </div>
+
             </div>
 
-            <div>
-            <HomeScreenMapCard>
-                dsa
-            </HomeScreenMapCard>
+            <div className=" container mx-auto px-4">
+                <div className="flex flex-row justify-between items-center mx-auto py-5 px-5">
+                    <HomeScreenMapCard/>
+                    
+                    <HomeScreenMapCard/>
+
+                    <HomeScreenMapCard/>
+            
             </div>
+            </div>
+
         </div>
+
+
     );
 }
 
