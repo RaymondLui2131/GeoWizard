9b6592254bdffc6bcbe33cede212d84d88e43a96
Jason <jchengjcheng52@gmail.com>
2023-11-08 16:33:21 -0500

    packages

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import LoginScreen from './components/LoginScreen'
 import RegisterScreen from './components/RegisterScreen'
 import HomeScreen from './components/HomeScreen'
+import EditUpload from './components/EditUploadScreen'
+import EditingMap from './components/EditingMapScreen'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/test" element={<LoginScreen />} />
         <Route path="/register" element={<RegisterScreen />} />
+        <Route path="/editUpload" element={<EditUpload />} />
+        <Route path="/editingMap" element={<EditingMap />} />
         <Route path="/" element={<HomeScreen />} />
+        
       </Routes>
     </BrowserRouter>
   )
