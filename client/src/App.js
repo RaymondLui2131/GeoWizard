66e392f4139fb176998770bff6bf39c93d1d3782
Raymond Lui <raymond.lui@stonybrook.edu>
2023-11-08 23:41:27 -0500

    added login and create account screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import LoginScreen from './components/LoginScreen'
+import Build1LoginScreen from './components/Build1LoginScreen'
 import RegisterScreen from './components/RegisterScreen'
 import HomeScreen from './components/HomeScreen'
 import EditUpload from './components/EditUploadScreen'
 import EditingMap from './components/EditingMapScreen'
+import LoginScreen from './components/LoginScreen'
+import CreateAccountScreen from './components/CreateAccountScreen'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/test" element={<LoginScreen />} />
+        <Route path="/test" element={<Build1LoginScreen />} />
         <Route path="/register" element={<RegisterScreen />} />
         <Route path="/editUpload" element={<EditUpload />} />
         <Route path="/editingMap" element={<EditingMap />} />
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/createAccount" element={<CreateAccountScreen />} />
         <Route path="/" element={<HomeScreen />} />
-        
       </Routes>
     </BrowserRouter>
   )
