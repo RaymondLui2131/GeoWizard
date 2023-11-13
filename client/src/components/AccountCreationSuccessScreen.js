5725b62da16f960e08a2e48225abe72e0a494599
Raymond Lui <raymond.lui@stonybrook.edu>
2023-11-13 13:47:32 -0500

    fixed pages centering

--- client/src/components/AccountCreationSuccessScreen.js
+++ client/src/components/AccountCreationSuccessScreen.js
@@ -5,15 +5,21 @@ const AccountCreationSuccessScreen = () => {
     return (
         <div className="min-h-screen max-h-screen bg-primary-GeoPurple">
             <Banner/>
-            <div className="container">
-                <div className="rectangle font-PyeongChangPeace-Light">
-                    <div className="pt-12 ">
-                        <img src={yellowCheck} className="h-10 sm:h-28 mx-auto" />
+
+            <div className='flex justify-center w-full'>
+
+                <div className="flex justify-center container">
+                    <div className="rectangle font-PyeongChangPeace-Light">
+                        <div className="pt-12 ">
+                            <img src={yellowCheck} className="h-10 sm:h-28 mx-auto" />
+                        </div>
+                        <div className='pt-12 text-2xl'>Account Created!</div>
+                        <div className='pt-12 text-base'>You have successfully created an account. You may now close this window.</div>
                     </div>
-                    <div className='pt-12 text-2xl'>Account Created!</div>
-                    <div className='pt-12 text-base'>You have successfully created an account. You may now close this window.</div>
                 </div>
+
             </div>
+
         </div>
     );
 }
--- client/src/components/FindEmailScreen.js
+++ client/src/components/FindEmailScreen.js
@@ -14,32 +14,42 @@ const FindEmailScreen = () => {
     return (
         <div className="min-h-screen max-h-screen bg-primary-GeoPurple">
             <Banner/>
-            <div className="container">
-                <div className="rectangle font-PyeongChangPeace-Light">
-                    <div className="pt-1 ">
-                        <img src={logo} className="h-10 sm:h-28 mx-auto" />
-                    </div>
-                    <div className='pt-4 text-3xl'>Find Your Email</div>
-                    <div className='pt-8 text-base'>Enter Your Recovery Email</div>
-                    
-                    <div className="pl-4 pt-4 flex flex-col justify-center items-center">
-                        <input
-                            className="text-black theEmail font-PyeongChangPeace-Light w-96 rounded-md py-2 border-solid border-2 border-gray-300 hover:border-primary-GeoPurple focus:border-primary-GeoPurple focus:outline-none "
-                            style={{ paddingLeft: '1rem', paddingRight: '1rem' }}
-                            value={userEmail}
-                            onChange={(e) => setUserEmail(e.target.value)}
-                            // onKeyUp={handleEmail}
-                        ></input>
-                    </div>
 
-                    <div className = "pt-8 pr-11 flex-col justify-center items-center">
-                        <button onClick={handleNextClick} className = "text-yellow-200 font-PyeongChangPeace-Bold rounded-md ml-10 py-2 px-6 border-solid border-2 border-gray-300 hover:bg-gray-300">
-                            Next
-                        </button>
-                    </div>
 
+            <div className='flex justify-center w-full'>
+                
+
+                <div className="flex justify-center container">
+                    <div className="rectangle font-PyeongChangPeace-Light ">
+                        <div className="pt-1 ">
+                            <img src={logo} className="h-10 sm:h-28 mx-auto" />
+                        </div>
+                        <div className='pt-4 text-3xl'>Find Your Email</div>
+                        <div className='pt-8 text-base'>Enter Your Recovery Email</div>
+                        
+                        <div className="pl-4 pt-4 flex flex-col justify-center items-center">
+                            <input
+                                className="text-black theEmail font-PyeongChangPeace-Light w-96 rounded-md py-2 border-solid border-2 border-gray-300 hover:border-primary-GeoPurple focus:border-primary-GeoPurple focus:outline-none "
+                                style={{ paddingLeft: '1rem', paddingRight: '1rem' }}
+                                value={userEmail}
+                                onChange={(e) => setUserEmail(e.target.value)}
+                                // onKeyUp={handleEmail}
+                            ></input>
+                        </div>
+
+                        <div className = "pt-8 pr-11 flex-col justify-center items-center">
+                            <button onClick={handleNextClick} className = "text-yellow-200 font-PyeongChangPeace-Bold rounded-md ml-10 py-2 px-6 border-solid border-2 border-gray-300 hover:bg-gray-300">
+                                Next
+                            </button>
+                        </div>
+
+                    </div>
                 </div>
+
+
             </div>
+
+
         </div>
     );
 }
