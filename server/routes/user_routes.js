98820586f1e3a41e79361f2897ea9582c7c707f5
kahui <kahuiw123@gmail.com>
2023-11-15 14:23:41 -0500

    implemented google login

--- server/routes/user_routes.js
+++ server/routes/user_routes.js
@@ -4,9 +4,10 @@
  */
 const express = require("express")
 const router = express.Router()
-const { registerUser, loginUser, getUser } = require("../controllers/user_controllers")
+const { registerUser, loginUser, getUser, googleLoginUser } = require("../controllers/user_controllers")
 const { verifyToken } = require("../jwt_middleware")
 router.post("/register", registerUser)
+router.post("/google/login", googleLoginUser)
 router.post("/login", loginUser)
 router.get("/me", verifyToken, getUser)
 module.exports = router
