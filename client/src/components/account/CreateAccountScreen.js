d6a787a5e3a46844ff68510afbc6e1f5fbb481d4
Raymond Lui <raymond.lui@stonybrook.edu>
2023-11-20 19:52:48 -0500

    useContext is set when a user registers

--- client/src/components/account/CreateAccountScreen.js
+++ client/src/components/account/CreateAccountScreen.js
@@ -1,11 +1,13 @@
 
 import logo from "../../assets/geowizlogo.png";
-import { useState } from 'react'
-import { postUser, checkUserEmail } from '../../api/auth_request_api.js';
+import { useState, useContext } from 'react'
+import { postUser, checkUserEmail, authloginUser } from '../../api/auth_request_api.js';
 import { useNavigate } from 'react-router-dom'
+import { UserContext, UserActionType } from "../../api/UserContext.js"
 
 const LoginScreen = () => {
     const navigate = useNavigate();
+    const {errorMessage, dispatch } = useContext(UserContext)
     const [userName, setUserName] = useState(''); // state for username
     const [userEmail, setUserEmail] = useState(''); // state for email
     const [password, setPassword] = useState(''); // state for password
@@ -65,10 +67,20 @@ const LoginScreen = () => {
             password: password
         }
 
+        const loginUser = async (e) => {
+            const response = await authloginUser(userEmail, password)
+            if (response.status == 200) {
+                dispatch({ type: UserActionType.LOGIN, payload: response.data })
+            } else {
+                dispatch({ type: UserActionType.ERROR, payload: response.data.message }) // login failed
+            }
+        }
+
         const postCreatedAccount = async () =>{
             try {
                 const response = await postUser(userData);
                 if (response.status === 200) {
+                    loginUser();
                     navigate('/createAccountSuccess'); 
                 }
                 else if (response.status === 400){
@@ -193,7 +205,9 @@ const LoginScreen = () => {
                         Create Account
                     </button>
                 </div>
-
+                {errorMessage && <p>
+                    {errorMessage}
+                </p>}
             </div>
         </div>
     );
