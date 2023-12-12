54507a9a5948bec73db1e0cc5357f8369f8d5d4a
kahui <kahuiw123@gmail.com>
2023-12-11 19:42:09 -0500

    updated EditUploadScreen layout and added file drop

--- client/src/components/EditUploadScreen.js
+++ client/src/components/EditUploadScreen.js
@@ -9,17 +9,13 @@ import france from "../assets/EditMapAssets/france.png"
 import ireland from "../assets/EditMapAssets/ireland.png"
 import finland from "../assets/EditMapAssets/finland.png"
 import poland from "../assets/EditMapAssets/poland.png"
-
 import franceGeoJson from '../assets/EditMapAssets/france-compress.geo.json';
 import irelandGeoJson from '../assets/EditMapAssets/ireland-compress.geo.json';
 import polandGeoJson from '../assets/EditMapAssets/poland-compress.geo.json';
 import finlandGeoJson from '../assets/EditMapAssets/finland-compress.geo.json';
-import index from "function.prototype.name";
-
 import toGeoJSON from '@mapbox/togeojson';
-import L from 'leaflet';
-import { truncate } from "lodash";
-import { feature } from "caniuse-lite";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFileArrowUp, faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 /* global shp */
 
 const DisplayMap = (props) => {
@@ -29,6 +25,7 @@ const DisplayMap = (props) => {
     const mapArr = props.arr
     const countryNames = ["France", "Ireland", "Finland", "Poland"]//To be changed
     const editClassName = countryNames[index]
+
     // const handleEditClick = () => {
     //     navigate('/editingmap')   //For now brings you back to / change later
     // }
@@ -64,11 +61,12 @@ const DisplayMap = (props) => {
     }
     return (
         <>
-            <div className="h-full">
-                <div className="bg-primary-GeoOrange text-center rounded-lg ">
-                    <img src={mapArr[index]} className="h-64 w-[28rem] rounded-lg" />
-                    <div className={editClassName} onClick={() => handleExistingMapsClick(countryNames[index])}>Edit {" " + countryNames[index]}</div>
-                </div>
+            <div className="h-full hover:opacity-70 hover:cursor-pointer shadow-aesthetic relative" onClick={() => handleExistingMapsClick(countryNames[index])}>
+                <img src={mapArr[index]} className="h-64 w-[28rem] rounded-lg relative" />
+                {/* <div className={editClassName}>Edit {" " + countryNames[index]}</div> */}
+                <span className={`${editClassName} absolute top-2 w-fit bg-primary-GeoOrange px-2 font-PyeongChangPeace-Light rounded-lg left-0 right-0 mx-auto opacity-70`}>
+                    {`Edit ${countryNames[index]}`}
+                </span>
             </div>
         </>
     )
@@ -86,7 +84,7 @@ const EditUpload = () => {
     const [mapIndex, setIndex] = useState(0); // index of mapArr
     const [mapArrayObj,] = useState([france, ireland, finland, poland]); // index of mapArr For now will just be array of imagepaths
     const [mapErrorMessage, setMapErrorMessage] = useState(false)
-
+    const [drag, setDrag] = useState(false)
     const navigate = useNavigate();
     const arrowOnclick = (direction) => {
         if (direction === 0)  //0 is left
@@ -107,8 +105,14 @@ const EditUpload = () => {
     const uploadHandle = () => {
         inputRef.current.click();
     }
-    const handleFileChange = (event) => {         //TODO parse file uploaded and move on to edit map setContext
-        const selected_file = event.target.files[0]
+    const handleFileChange = (event) => {
+        event.preventDefault()
+        let selected_file
+        if (event.dataTransfer?.files[0] instanceof File) {
+            selected_file = event.dataTransfer.files[0]
+        } else {
+            selected_file = event.target.files[0]
+        }
         const file_type = selected_file.name.split('.').pop().toLowerCase()
         console.log(file_type)
         const options = { tolerance: 0.01, highQuality: true }
@@ -223,59 +227,81 @@ const EditUpload = () => {
                 setMapErrorMessage(true)
         }
     }
+
+    const handleDragOver = (e) => {
+        e.preventDefault()
+        setDrag(true)
+    }
+
+    const handleDragLeave = (e) => {
+        e.preventDefault()
+        setDrag(false)
+    }
+
     return (
         <>
-            <div className="bg-primary-GeoPurple min-h-screen max-h-screen overflow-auto">
-                <div className="flex justify-center pl-8 md:pl-10 lg:pl-20 xl:pl-40 pr-8 md:pr-10 lg:pr-20 xl:pr-40">
-                    <div className="flex flex-col">
-                        <div className="flex justify-between pt-10 ">
-                            <div className=" text-primary-GeoBlue font-NanumSquareNeoOTF-Bd">
-                                <h3 className="text-3xl pb-5">Start Editing Maps by uploading our supported map formats :</h3>
-                                <ul className="list-disc pl-5 ">
-                                    <li className="text-xl font-NanumSquareNeoOTF-Lt mb-5">GeoJson</li>
-                                    <li className="text-xl font-NanumSquareNeoOTF-Lt mb-5">KML</li>
-                                    <li className="text-xl font-NanumSquareNeoOTF-Lt mb-5">SHP and DBFS upload as zip</li>
-                                    <li className="text-xl font-NanumSquareNeoOTF-Lt mb-5">GeoWizJson</li>
-                                </ul>
-                            </div>
-                            <div className=" pt-20 ">
-                                <input className="hidden" type="file" accept=".zip, .json, .kml, .shp, .geowizjson" ref={inputRef} onChange={handleFileChange} />
-                                <div className="group flex relative">
-                                    <button data-test-id="upload-button" className="bg-primary-GeoOrange px-16 rounded-full py-2 disabled:opacity-30" onClick={() => uploadHandle()} disabled={!user}>
-                                        Upload
-                                    </button>
-                                    {!user && <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 whitespace-nowrap rounded-md absolute left-1/2 
-    -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">You must be logged in to upload a map</span>}
+            <div className="bg-primary-GeoPurple max-h-[100%] min-h-screen overflow-auto">
+                <div className="flex h-full mt-10 min-w-full flex-col justify-center md:pl-10 lg:pl-20 xl:pl-40 md:pr-10 lg:pr-20 xl:pr-40">
+                    <div className="flex flex-col h-full justify-evenly">
+                        <div className="flex flex-col items-center">
+                            <div className="flex flex-col justify-evenly h-1/2">
+                                <div className=" text-primary-GeoBlue font-NanumSquareNeoOTF-Bd">
+                                    <h3 className="text-3xl pb-5 text-center whitespace-nowrap">Start Editing Maps by uploading our supported map formats :</h3>
+                                    <ul className="flex justify-evenly list-disc pl-5 ">
+                                        <li className="text-xl font-NanumSquareNeoOTF-Lt mb-5">GeoJson</li>
+                                        <li className="text-xl font-NanumSquareNeoOTF-Lt mb-5">KML</li>
+                                        <li className="text-xl font-NanumSquareNeoOTF-Lt mb-5">SHP and DBFS upload as zip</li>
+                                        <li className="text-xl font-NanumSquareNeoOTF-Lt mb-5">GeoWizJson</li>
+                                    </ul>
                                 </div>
-
-                                {mapErrorMessage ?
-                                    <div style={{ color: '#8B0000', textAlign: 'center' }}> Please upload a supported format </div>
-                                    : null}
                             </div>
+
+                            <div className={`relative group flex justify-center w-4/5 h-64 shadow-sleek rounded-md ${user && 'hover:border hover:border-primary-GeoBlue'} ${drag && user && "border border-primary-GeoBlue"}`}>
+                                <button className="h-full w-full disabled:opacity-30 hover:opacity-80" data-test-id="upload-button" onDragOver={handleDragOver} onDrop={handleFileChange} onDragLeave={handleDragLeave} onClick={() => uploadHandle()} disabled={!user}>
+                                    <div className="h-full flex flex-col justify-center items-center gap-5">
+                                        <input className="hidden" type="file" accept=".zip, .json, .kml, .shp, .geowizjson" ref={inputRef} onChange={handleFileChange} />
+                                        <FontAwesomeIcon className='h-1/4' icon={faFileArrowUp} />
+                                        <div className="font-PyeongChangPeace-Light group flex w-full justify-center">
+                                            <p
+                                                className=" shadow-aesthetic bg-primary-GeoOrange w-1/2 px-16 rounded-md text-3xl py-2"
+                                            >
+                                                Select a File
+                                            </p>
+                                        </div>
+                                        {mapErrorMessage ? (
+                                            <div style={{ color: '#8B0000', textAlign: 'center' }}> Please upload a supported format </div>
+                                        ) : null}
+                                    </div>
+                                </button>
+                                {!user && (
+                                    <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 whitespace-nowrap py-1 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">
+                                        You must be logged in to upload a map
+                                    </span>
+                                )}
+                            </div>
+
                         </div>
-                        <div className="flex flex-col pt-16">
-                            <div className="text-3xl font-NanumSquareNeoOTF-Bd">
+
+                        <div className="font-NanumSquareNeoOTF-Bd flex flex-col text-center mt-10 pb-10">
+                            <div className="text-3xl">
                                 OR
                             </div>
 
-                            <div className=" text-xl pt-10 font-NanumSquareNeoOTF-Lt  text-primary-GeoBlue">
-                                Start From a Selection of Existing Maps
+                            <div className=" text-3xl pt-6   text-primary-GeoBlue">
+                                Start From a Selection of Existing Maps:
                             </div>
-                            <div className="flex flex-row justify-around pt-10 items-center" >
-                                <div className="w-20 h-20 rounded-full border border-opacity-100 border-black bg-primary-GeoBlue
-                                text-center text-3xl flex justify-center items-center mt-12 font-extrabold"
-                                    onClick={() => arrowOnclick(0)}>
-                                    ←
-                                </div>
+                            <div className="flex flex-row justify-around pt-10 items-center gap-8" >
+                                <button className="hover:opacity-70 px-5" onClick={() => arrowOnclick(0)}>
+                                    <FontAwesomeIcon className="h-10" icon={faChevronLeft} />
+                                </button>
                                 <DisplayMap {...{ index: mapIndex, arr: mapArrayObj }} />
                                 <DisplayMap {...{ index: mapIndex + 1, arr: mapArrayObj }} />
-                                <div className="w-20 h-20 rounded-full border border-opacity-100 border-black bg-primary-GeoBlue
-                                text-center text-3xl flex justify-center items-center mt-12 font-extrabold"
-                                    onClick={() => arrowOnclick(1)}>
-                                    →
-                                </div>
+                                <button className="next hover:opacity-70 px-5" onClick={() => arrowOnclick(1)}>
+                                    <FontAwesomeIcon className="h-10" icon={faChevronRight} />
+                                </button>
                             </div>
                         </div>
+
                     </div>
                 </div>
             </div>
